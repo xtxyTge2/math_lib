b0901e86df5fcb454527cc781429f0f16dbbe86b
xtxyTge2 <f1AZnn@protonmail.com>
2023-04-05 14:27:16 +0200

    Add first documentation.

--- src/rotations.cpp
+++ src/rotations.cpp
@@ -1,6 +1,35 @@
 #include "rotations.hpp"
 
-Vec3f ZYZ_angles_from_rotation_matrix(const Matrix3x3f& R) {
+
+/*
+	Computes the Euler-Angles in the "ZX'Z''"-convention from a rotation matrix R (that is, R has orthogonal and unit-length column (or equivalently row) vectors). 
+
+	The method returns a vector Vec3f v of three floating point values, such that with the terminology, 
+	alpha = v.x
+	beta = v.y,
+	gamma = v.z, 
+
+	it holds that 
+	
+	R = R_z(gamma) * R_x(beta) * R_z(alpha),
+	
+	where R_x(a), R_y(a), R_z(a) denote the following matrices, representing a rotation of an angle a around the x, y, respectively the z axis:
+
+	R_x(a) = |1 	0      	0	   |
+			 |0 	cos(x) 	-sin(x)|
+			 |0		sin(x)	cos(x) |	
+
+	R_y(a) = |cos(x) 	0	sin(x)|
+			 |0 		1 	0	  |
+			 |-sin(x)	0	cos(x)|
+
+	R_z(a) = |cos(x) 	-sin(x)	0 |
+			 |sin(x) 	cos(x) 	0 |
+			 |0			0		1 |.
+
+	The angles alpha, beta, gamma are not unique.
+*/
+Vec3f ZXZ_angles_from_rotation_matrix(const Matrix3x3f& R) {
 	float alpha = 0.0f;
 	float beta = 0.0f;
 	float gamma = 0.0f;
