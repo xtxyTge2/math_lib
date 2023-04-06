957b8138500f0aa9048cdb13e7797e99f81514ea
xtxyTge2 <f1AZnn@protonmail.com>
2023-04-06 12:14:15 +0200

    Add quaternions class with multiplication operation.

---
+++ src/quaternions.cpp
@@ -0,0 +1,12 @@
+#include "quaternions.hpp"
+
+
+Quaternion operator*(const Quaternion& q, const Quaternion& p)
+{
+	float r = q.r * p.r + q.i * p.i + q.j * p.j + q.k * p.k;
+	float i = q.r * p.i + q.i * p.r + q.j * p.k - q.k * p.j;
+	float j = q.r * p.j - q.i * p.k + q.j * p.r + q.k * p.i;
+	float k = q.r * p.k + q.i * p.j - q.j * p.i + q.k * p.r;
+
+	return Quaternion(r, i, j, k);
+}
---
+++ src/quaternions.hpp
@@ -0,0 +1,34 @@
+
+
+class Quaternion {
+	Quaternion() = default;
+
+	Quaternion(float a_r) :
+		r(a_r)
+		, i(0.0f)
+		, j(0.0f)
+		, k(0.0f)
+	{};
+
+	Quaternion(Vec3f v) :
+		r(0.0f)
+		, i(v.x)
+		, j(v.y)
+		, k(v.z)
+	{};
+
+	Quaternion(float a_r, float a_i, float a_j, float a_k) :
+		r(a_r)
+		, i(a_i)
+		, j(a_j)
+		, k(a_k)
+	{};
+
+private:
+	float r;
+	float i;
+	float j;
+	float k;
+};
+
+Quaternion operator*(const Quaternion& q, const Quaternion& p);
